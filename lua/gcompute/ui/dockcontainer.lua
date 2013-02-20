59dfe18476ebaba5d0b5ecdbcf324032780b4a53
!cake <cakenotfound@googlemail.com>
2013-02-20 16:49:41 +0000

    [GCompute] Fixed include path in ui/dockcontainer.lua.

--- lua/gcompute/ui/dockcontainer.lua
+++ lua/gcompute/ui/dockcontainer.lua
@@ -1,7 +1,7 @@
 GCompute.DockContainer = {}
 
 include ("dockcontainer/dockcontainer.lua")
-include ("dockcontainer/dockcontainerdroptarget.lua")
 include ("dockcontainer/dockcontainertype.lua")
 include ("dockcontainer/dockingglyphs.lua")
 include ("dockcontainer/dockingside.lua")
+include ("dockcontainer/droptarget.lua")

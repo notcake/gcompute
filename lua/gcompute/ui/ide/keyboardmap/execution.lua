114704da6a15a3ea2a477edd31beae6cff4ce8e1
!cake <cakenotfound@googlemail.com>
2013-04-08 00:54:44 +0000

    [GCompute] Bound all of F1 to F12 to run code in the IDE.

--- lua/gcompute/ui/ide/keyboardmap/execution.lua
+++ lua/gcompute/ui/ide/keyboardmap/execution.lua
@@ -1,4 +1,4 @@
-GCompute.IDE.KeyboardMap:Register (KEY_F5,
+GCompute.IDE.KeyboardMap:Register ({ KEY_F1, KEY_F2, KEY_F3, KEY_F4, KEY_F5, KEY_F6, KEY_F7, KEY_F8, KEY_F9, KEY_F10, KEY_F11, KEY_F12 },
 	function (self, key, ctrl, shift, alt)
 		self:DispatchAction ("Run Code")
 	end

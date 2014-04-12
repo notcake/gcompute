686660acd2ea8e972c2ad229617cd8959c84206b
notcake <cakenotfound@gmail.com>
2014-04-12 03:21:53 +0100

    [GCompute] Removed inappropriate caching in DefaultTypePrinter.

--- lua/gcompute/glua/printing/defaulttypeprinter.lua
+++ lua/gcompute/glua/printing/defaulttypeprinter.lua
@@ -2,30 +2,17 @@ local self = {}
 GCompute.GLua.Printing.DefaultTypePrinter = GCompute.MakeConstructor (self, GCompute.GLua.Printing.TypePrinter)
 
 function self:ctor ()
-	self.ObjectString = nil
-end
-
--- Caching
-function self:InvalidateCache ()
-	self.ObjectString = nil
 end
 
 -- Printing
 function self:Measure (printer, obj, printingOptions, alignmentController)
-	if self:GetCacheObject () ~= obj then
-		self:SetCache (printer, obj, printingOptions)
-		self.ObjectString = GLib.Lua.ToLuaString (obj)
-	end
-	return #self.ObjectString
+	return #GLib.Lua.ToLuaString (obj)
 end
 
 function self:Print (printer, coloredTextSink, obj, printingOptions, alignmentController)
-	if self:GetCacheObject () ~= obj then
-		self:SetCache (printer, obj, printingOptions)
-		self.ObjectString = GLib.Lua.ToLuaString (obj)
-	end
-	coloredTextSink:WriteColor (self.ObjectString, GLib.Colors.White)
-	return #self.ObjectString
+	local str = GLib.Lua.ToLuaString (obj)
+	coloredTextSink:WriteColor (str, GLib.Colors.White)
+	return #str
 end
 
 GCompute.GLua.Printing.DefaultTypePrinter = GCompute.GLua.Printing.DefaultTypePrinter ()

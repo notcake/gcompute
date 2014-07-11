e7ed9f4f225fa11af2391e7d3302310880b59fb5
notcake <cakenotfound@gmail.com>
2014-07-11 17:18:36 +0100

    [GCompute] Added a load of aowl commands.

--- lua/gcompute/interop/aowl.lua
+++ lua/gcompute/interop/aowl.lua
@@ -1,56 +1,84 @@
 if not SERVER then return end
 
 local function RegisterAowlCommands ()
-	local commands =
+	local function ExecuteExpression (ownerId, hostId, expression)
+		local executionContext, returnCode = GCompute.Execution.ExecutionService:CreateExecutionContext (ownerId, hostId, "GLua", GCompute.Execution.ExecutionContextOptions.EasyContext + GCompute.Execution.ExecutionContextOptions.Repl)
+		
+		if not executionContext then
+			print ("Failed to create execution context (" .. GCompute.ReturnCode [returnCode] .. ").")
+			return
+		end
+		
+		local executionInstance, returnCode = executionContext:CreateExecutionInstance (expression, nil, GCompute.Execution.ExecutionInstanceOptions.EasyContext + GCompute.Execution.ExecutionInstanceOptions.ExecuteImmediately + GCompute.Execution.ExecutionInstanceOptions.CaptureOutput + GCompute.Execution.ExecutionInstanceOptions.SuppressHostOutput)
+		
+		if executionInstance then
+			executionInstance:GetCompilerStdOut ():ChainTo (GCompute.Text.ConsoleTextSink)
+			executionInstance:GetCompilerStdErr ():ChainTo (GCompute.Text.ConsoleTextSink)
+			executionInstance:GetStdOut ():AddEventListener ("Text",
+				function (_, text, color)
+					color = color or GLib.Colors.White
+					
+					-- Translate color
+					local colorId = GCompute.SyntaxColoring.PlaceholderSyntaxColoringScheme:GetIdFromColor (color)
+					if colorId then
+						color = GCompute.SyntaxColoring.DefaultSyntaxColoringScheme:GetColor (colorId) or color
+					end
+					
+					GCompute.Text.ConsoleTextSink:WriteColor (text, color)
+				end
+			)
+			executionInstance:GetStdErr ():ChainTo (GCompute.Text.ConsoleTextSink)
+			
+			-- Line break
+			GCompute.Text.ConsoleTextSink:WriteOptionalLineBreak ()
+			
+			executionInstance:dtor ()
+			executionInstance = nil
+		else
+			print ("Failed to create execution instance (" .. GCompute.ReturnCode [returnCode] .. ").")
+		end
+		
+		executionContext:dtor ()
+		executionContext = nil
+	end
+	
+	local executionCommands =
 	{
-		"print2",
-		"p",
-		"tbl",
-		"table2"
+		["p"      ] = GLib.GetServerId (),
+		["t2"     ] = GLib.GetServerId (),
+		["tbl"    ] = GLib.GetServerId (),
+		["print2" ] = GLib.GetServerId (),
+		["table2" ] = GLib.GetServerId (),
+		
+		["pc"     ] = "Clients",
+		["tc2"    ] = "Clients",
+		["tblc"   ] = "Clients",
+		["printc2"] = "Clients",
+		["tablec" ] = "Clients",
+		["tablec2"] = "Clients",
+		
+		["ps"     ] = "Shared",
+		["ts"     ] = "Shared",
+		["tbls"   ] = "Shared",
+		["prints" ] = "Shared",
+		["tables" ] = "Shared",
+		["prints2"] = "Shared",
+		["tables2"] = "Shared",
+		
+		["pm2"    ] = "^",
+		["tm"     ] = "^",
+		["tblm"   ] = "^",
+		["printm2"] = "^",
+		["tablem" ] = "^",
+		["tablem2"] = "^"
 	}
 	
-	for _, command in ipairs (commands) do
+	for command, hostId in pairs (executionCommands) do
 		aowl.AddCommand (
 			command,
 			function (ply, expression)
-				local executionContext, returnCode = GCompute.Execution.ExecutionService:CreateExecutionContext (GLib.GetPlayerId (ply), GLib.GetServerId (), "GLua", GCompute.Execution.ExecutionContextOptions.EasyContext + GCompute.Execution.ExecutionContextOptions.Repl)
-				
-				if not executionContext then
-					print ("Failed to create execution context (" .. GCompute.ReturnCode [returnCode] .. ").")
-					return
-				end
-				
-				local executionInstance, returnCode = executionContext:CreateExecutionInstance (expression, nil, GCompute.Execution.ExecutionInstanceOptions.EasyContext + GCompute.Execution.ExecutionInstanceOptions.ExecuteImmediately + GCompute.Execution.ExecutionInstanceOptions.CaptureOutput + GCompute.Execution.ExecutionInstanceOptions.SuppressHostOutput)
-				
-				if executionInstance then
-					executionInstance:GetCompilerStdOut ():ChainTo (GCompute.Text.ConsoleTextSink)
-					executionInstance:GetCompilerStdErr ():ChainTo (GCompute.Text.ConsoleTextSink)
-					executionInstance:GetStdOut ():AddEventListener ("Text",
-						function (_, text, color)
-							color = color or GLib.Colors.White
-							
-							-- Translate color
-							local colorId = GCompute.SyntaxColoring.PlaceholderSyntaxColoringScheme:GetIdFromColor (color)
-							if colorId then
-								color = GCompute.SyntaxColoring.DefaultSyntaxColoringScheme:GetColor (colorId) or color
-							end
-							
-							GCompute.Text.ConsoleTextSink:WriteColor (text, color)
-						end
-					)
-					executionInstance:GetStdErr ():ChainTo (GCompute.Text.ConsoleTextSink)
-					
-					-- Line break
-					GCompute.Text.ConsoleTextSink:WriteOptionalLineBreak ()
-					
-					executionInstance:dtor ()
-					executionInstance = nil
-				else
-					print ("Failed to create execution instance (" .. GCompute.ReturnCode [returnCode] .. ").")
-				end
-				
-				executionContext:dtor ()
-				executionContext = nil
+				local userId = GLib.GetPlayerId (ply)
+				ExecuteExpression (userId, hostId == "^" and userId or hostId, expression)
 			end,
 			"developers"
 		)
